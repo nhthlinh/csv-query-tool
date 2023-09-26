e88535b3955facfb71923a8be41c7d29fa6d720a
nhthlinh <142315819+nhthlinh@users.noreply.github.com>
2023-09-26 11:11:08 +0700

    Ket o phan op la >= hoac <=

--- source.cpp
+++ source.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 string statement;
 
-enum TokenType {ACTION, INTO, FROM, VALUES, WHERE, FILENAME, ID, LIKE};
+enum TokenType {ACTION, INTO, FROM, VALUES, WHERE, FILENAME, ID, OPERATION, SORT_BY, ORDER_BY, RELATION, OPEN, CLOSE};
 
 struct Token {
     TokenType type;
@@ -26,7 +26,12 @@ string tokentype_name(Token T) {
         case 4: return "WHERE";
         case 5: return "FILENAME";
         case 6: return "ID";
-        case 7: return "LIKE";
+        case 7: return "OPERATION";
+        case 8: return "SORT_BY";
+        case 9: return "ORDER_BY";
+        case 10: return "RELATION";
+        case 11: return "OPEN";
+        case 12: return "CLOSE";
     }
     return "";
 }
@@ -34,7 +39,8 @@ string tokentype_name(Token T) {
 vector<Token> extract(string input) {
     vector<Token> tokens;
 
-    regex tokenRegex("INSERT|SELECT|INTO|FROM|LIKE|VALUES|\\w+\.csv|([\"].*?[\"0-9]+|A-Za-z0-9]+)|\\w+\.\\w+|\\w+");
+    regex tokenRegex(R"(\b(INSERT|SELECT|INTO|FROM|LIKE|VALUES|WHERE|ORDER\sBY|ASC|DESC|[*()]|(\S+\.csv))\b|\\w+\.\\w+|([\"].*?[\"0-9]+|A-Za-z0-9]+)|\b\w+\b|('[^']*')|("%[^%]*%")|[><=*]|\(|\))");
+    //regex tokenRegex("INSERT|SELECT|INTO|FROM|LIKE|VALUES|\\w+\.csv|([\"].*?[\"0-9]+|A-Za-z0-9]+)||\\w+");
 
     sregex_iterator it(input.begin(), input.end(), tokenRegex);
     sregex_iterator end;
@@ -53,14 +59,17 @@ vector<Token> extract(string input) {
         else if (token.value == "FROM") token.type = FROM;
         else if (token.value == "VALUES") token.type = VALUES;
         else if (token.value == "WHERE") token.type = WHERE;
-        else if (token.value == "LIKE") token.type = LIKE;
+        else if (token.value == ">" || token.value == "<" || token.value == "=" || token.value == "LIKE") token.type = OPERATION;
+        else if (token.value == "ORDER BY") token.type = ORDER_BY;
+        else if (token.value == "ASC" || token.value == "DESC") token.type = SORT_BY;
+        else if (token.value == "AND" || token.value == "OR") token.type = RELATION;
         else if (token.value.length() >= 5) {
             if (token.value.substr(token.value.length()-4) == ".csv") token.type = FILENAME;
             else token.type = ID;
         }
+        else if (token.value == "(") token.type = OPEN;
+        else if (token.value == ")") token.type = CLOSE;
         else token.type = ID;
-
-
         tokens.push_back(token);
         ++it;
     }
@@ -121,6 +130,10 @@ public:
     short int op;
     string value;
 public:
+    RelationalExpr(string colName, string op, string value) {
+        this->colName = colName;
+        this->value = value;
+    }
     string toString() const {
         // TODO
         return colName + " " + to_string(op) + value;
@@ -167,54 +180,34 @@ public:
         this->columnNames.clear();
         this->values.clear();
 
-        if (T.size() <= 3 || T[0].value != "INSERT" || T[1].type != INTO || T[2].type != FILENAME) cout<<"ERROR!";
+        if (T.size() <= 3 || T[0].value != "INSERT" || T[1].type != INTO || T[2].type != FILENAME) this->error();
         else if (T[2].type == FILENAME) {
             this->filename = T[2].value;
-
-            int i = 3, j = 0;
-            if (T[3].type == ID) {
-                while (T[i].type == ID) { 
-                    this->columnNames.push_back(T[i].value); 
-                    ++i;
+            int i = 4, j = 0;
+            if (T[3].type == OPEN && T[4].type == ID) {
+                while (T[i].type == ID) this->columnNames.push_back(T[i++].value);
+                if (T[i].type == CLOSE && T[i+=1].type == VALUES && T[i+=1].type == OPEN) {
+                    i+=1;
+                    while (T[i].type == ID) this->values.push_back(T[i++].value);
                 }
-                if (T[i].type == VALUES) {
-                    i += 1;
-                    while (T[i].type == ID) {
-                        this->values.push_back(T[i].value);
-                        ++i;
-                    }
-                }
-                else {
-                    cout<<"ERROR!";
-                    this->filename = "";
-                    this->columnNames.clear();
-                    this->values.clear();
-                }
+                else this->error();
             }
             else if (T[i].type == VALUES) {
                 i += 1;
                 if (T[i].type == ID) {
-                    while (T[i].type == ID) {
-                        this->values.push_back(T[i].value);
-                        ++i;
-                    }
+                    while (T[i].type == ID) this->values.push_back(T[i++].value);
                 }
-                else {
-                    cout<<"ERROR!";
-                    this->filename = "";
-                    this->columnNames.clear();
-                    this->values.clear();
-                }
+                else this->error();
             }
-            else {
-                cout<<"ERROR!";
-                this->filename = "";
-                this->columnNames.clear();
-                this->values.clear();
-            }
+            else this->error();
         }
-
         if (this->check_condition()) this->do_action();
+    }
+    void error() {
+        cout<<"ERROR!";
+        this->filename = "";
+        this->columnNames.clear();
+        this->values.clear();
     }
     string toString() const {
         string ans = "";
@@ -304,17 +297,93 @@ public:
         this->expr = nullptr;
         this->sort = nullptr;
 
-        if (T[0].value == "SELECT") {
+        if (T[2].type == ID && T[1].type == OPEN) {
+            int i = 2;
+            for ( ; T[i].type == ID; i++) this->columnNames.push_back(T[i].value);
+            if (T[i].type == CLOSE && T[i+=1].type == FROM) {
+                i+=1;
 
+                if (T[i].type == FILENAME) this->dataSource = &T[i++].value;
+                else if (T[i].type == OPEN && T[i+=1].value == "SELECT") { 
+                    int open = 1, close = 0, from = 0, where = 0; 
+                    vector<Token> source; 
+                    for (; i<T.size(); i++) {
+                        if (T[i].type == OPEN) open++;
+                        if (T[i].type == CLOSE) close++;
+                        if (T[i].type == FROM) from++;
+                        if (T[i].type == WHERE) where++;
+                        if (from >= 2 || where >=2) break;
+                        source.push_back(T[i]);
+                    }
+                    if (open != close) this->error();
+                    else this->dataSource = new SelectCommand(source);
+                }
+                else this->error();
+    
+                if (i < T.size()) {
+                    vector<Token> condition;
+                    while (i!=T.size()) {
+                        condition.push_back(T[i++]);
+                    }
+                    this->buile_condition(condition);
+                }
+                else {
+                    this->expr = nullptr;
+                    this->sort = nullptr;
+                }
+                
+            }
+            else this->error();
         }
-        else cout<<"ERROR!";
+        else this->error();
+        //this->toString();
     }
+    void error() {
+        cout<<"ERROR!";
+        this->columnNames.clear();
+        this->dataSource = nullptr;
+        this->expr = nullptr;
+        this->sort = nullptr;
+    } 
     string toString() const {
-        // TODO
+        string s = "";
+        int i = 0;
+        while (!this->columnNames.empty()) {
+            cout<<this->columnNames[i]<<endl;
+            i++;
+        }
         return "";
     }
     void do_action() {
 
+    }
+    void buile_condition(vector<Token> T) {
+        int i = 1;
+        if (T[0].type == WHERE) {
+            for ( ; i<T.size(); i++) {
+                if (T[i].type == OPEN) i++;
+                if (T[i].type == ID) {
+                    string coln = T[i].value; cout<<coln<<endl;
+
+                    if (T[i+=1].type == OPERATION && T[i+=1].type == ID) {
+                        string value = T[i].value; cout<<value<<endl;
+                        string op = T[i-=1].value; cout<<op<<endl;
+                        i+=1;
+                        
+                        //this->expr = new RelationalExpr(coln, op, value);
+                    }
+                    else if (T[i+=1].type == OPERATION && T[i+=1].type == OPERATION && T[i+=1].type == ID) {
+                        string value = T[i].value; cout<<value<<endl;
+                        string op = T[i-=1].value; cout<<op<<endl;
+                        i+=1;
+                    }
+                    else this->error();
+                }
+            }
+        }
+        if (T[i++].type == ORDER_BY) {
+                
+        }
     }
 };
 
@@ -347,7 +416,7 @@ int main(int argc, char* argv[]) {
     vector<Token> tokens = extract(statement);
 
 int i = 0;
-    for (Token token:tokens) {
+    for (Token token : tokens) {
         cout<<i++<<"---" << tokentype_name(token) << "----" <<token.value <<"\n";
     }
 
